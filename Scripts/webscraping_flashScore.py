f21523af2d362dfee3c38019a087f73338021e24
Irineu Rosa Junior <billzte@gmail.com>
2023-07-16 17:08:17 -0300

    Atualização 16/07

--- Scripts/webscraping_flashScore.py
+++ Scripts/webscraping_flashScore.py
@@ -26,58 +26,6 @@ def grafico(df, nome):
     print("Profit:",profit,"stakes em", len(df),"jogos")
     print("ROI:",ROI,"%")
 
-# %% [markdown]
-# ## Instalando as Dependências do Ubuntu
-
-# %%
-# %%shell
-# # Ubuntu no longer distributes chromium-browser outside of snap
-# #
-# # Proposed solution: https://askubuntu.com/questions/1204571/how-to-install-chromium-without-snap
-
-# # Add debian buster
-# cat > /etc/apt/sources.list.d/debian.list <<'EOF'
-# deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster.gpg] http://deb.debian.org/debian buster main
-# deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster-updates.gpg] http://deb.debian.org/debian buster-updates main
-# deb [arch=amd64 signed-by=/usr/share/keyrings/debian-security-buster.gpg] http://deb.debian.org/debian-security buster/updates main
-# EOF
-
-# # Add keys
-# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
-# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
-
-# apt-key export 77E11517 | gpg --dearmour -o /usr/share/keyrings/debian-buster.gpg
-# apt-key export 22F3D138 | gpg --dearmour -o /usr/share/keyrings/debian-buster-updates.gpg
-# apt-key export E562B32A | gpg --dearmour -o /usr/share/keyrings/debian-security-buster.gpg
-
-# # Prefer debian repo for chromium* packages only
-# # Note the double-blank lines between entries
-# cat > /etc/apt/preferences.d/chromium.pref << 'EOF'
-# Package: *
-# Pin: release a=eoan
-# Pin-Priority: 500
-
-# Package: *
-# Pin: origin "deb.debian.org"
-# Pin-Priority: 300
-
-# Package: chromium*
-# Pin: origin "deb.debian.org"
-# Pin-Priority: 700
-# EOF
-
-# # Install chromium and chromium-driver
-# apt-get update
-# apt-get install chromium chromium-driver
-
-# # Install selenium
-# pip install selenium
-
-# %% [markdown]
-# ## Configuração do Web-Driver
-
-# %%
 import time
 from tqdm import tqdm
 from selenium import webdriver
@@ -92,14 +40,9 @@ options.headless = True
 from selenium.webdriver.chrome.service import Service
 wd_Chrome = webdriver.Chrome("/usr/bin/chromedriver", options=options)
 
-# %% [markdown]
-# ## Iniciando a Raspagem de Dados
-
-# %%
 # Com o WebDrive a gente consegue a pedir a página (URL)
 wd_Chrome.get("https://www.flashscore.com/")
 
-# %%
 # Fechando Botão de Cookies
 try:
     button_cookies = wd_Chrome.find_element(By.CSS_SELECTOR,'button#onetrust-accept-btn-handler')
